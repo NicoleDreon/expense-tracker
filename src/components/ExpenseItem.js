c024fc2da8d1d629a5208b4ff5cd05773d1ae08f
Nicole Dreon <nicole.dreon@perficient.com>
2021-06-29 15:40:14 -0400

    add placeholder information

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insuance</h2>
+        <div>$250.00</div>
+      </div>
+    </div>
+  );
 }
-
 export default ExpenseItem;
